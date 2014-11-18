2850baad11c3c5d22ee08aca9355f58184f55c92
Markus Hoelzle <typo3@markus-hoelzle.de>
2014-11-18 14:49:33 +0100

    [BUGFIX] Register driver with DriverRegistry

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,11 +3,13 @@ if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
-// register additional driver
-$TYPO3_CONF_VARS['SYS']['fal']['registeredDrivers'][\AUS\AusDriverAmazonS3\Driver\AmazonS3Driver::DRIVER_TYPE] = array(
-	'class' => 'AUS\AusDriverAmazonS3\Driver\AmazonS3Driver',
-	'label' => 'Amazon S3',
-	'flexFormDS' => 'FILE:EXT:' . \AUS\AusDriverAmazonS3\Driver\AmazonS3Driver::EXTENSION_KEY . '/Configuration/FlexForm/AmazonS3DriverFlexForm.xml'
+/** @var \TYPO3\CMS\Core\Resource\Driver\DriverRegistry $driverRegistry */
+$driverRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Resource\\Driver\\DriverRegistry');
+$driverRegistry->registerDriverClass(
+	'AUS\AusDriverAmazonS3\Driver\AmazonS3Driver',
+	\AUS\AusDriverAmazonS3\Driver\AmazonS3Driver::DRIVER_TYPE,
+	'Amazon S3',
+	'FILE:EXT:' . \AUS\AusDriverAmazonS3\Driver\AmazonS3Driver::EXTENSION_KEY . '/Configuration/FlexForm/AmazonS3DriverFlexForm.xml'
 );
 
 // register extractor

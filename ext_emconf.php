6f1e5d21a071b2a96463907901508af082393490
Markus Hoelzle <typo3@markus-hoelzle.de>
2015-04-27 15:22:09 +0200

    [TASK] Release of 1.3.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,13 +4,13 @@ $EM_CONF[$_EXTKEY] = array (
 	'title' => 'anders und sehr: Amazon S3 FAL driver (CDN)',
 	'description' => 'Provides a FAL driver for the Amazon Web Service S3.',
 	'category' => 'be',
-	'version' => '1.2.0',
+	'version' => '1.3.0',
 	'state' => 'stable',
 	'uploadfolder' => false,
 	'createDirs' => '',
 	'clearcacheonload' => false,
 	'author' => 'Markus Hoelzle',
-	'author_email' => 'm.hoelzle@andersundsehr.com',
+	'author_email' => 'typo3@markus-hoelzle.de',
 	'author_company' => 'anders und sehr GmbH',
 	'constraints' => 
 	array (
@@ -18,11 +18,7 @@ $EM_CONF[$_EXTKEY] = array (
 		array (
 			'typo3' => '6.2.0-7.99.99',
 		),
-		'conflicts' => 
-		array (
-		),
-		'suggests' => 
-		array (
-		),
+		'conflicts' => array (),
+		'suggests' => array (),
 	),
 );

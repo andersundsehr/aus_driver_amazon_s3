ea452ee895e5bae29fbc8679f5ad4984a58b7148
Markus Hoelzle <m.hoelzle@andersundsehr.com>
2017-01-05 11:11:01 +0100

    [TASK] Release version 1.5.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,11 +1,10 @@
 <?php
-
 /** @var string $_EXTKEY */
 $EM_CONF[$_EXTKEY] = array(
     'title' => 'anders und sehr: Amazon AWS S3 FAL driver (CDN)',
     'description' => 'Provides a FAL driver for the Amazon Web Service S3.',
     'category' => 'be',
-    'version' => '1.5.0',
+    'version' => '1.5.1',
     'state' => 'stable',
     'uploadfolder' => false,
     'createDirs' => '',

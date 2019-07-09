0c1934a64defc3b1638303e27961f09393b6cf6f
Markus Hoelzle <mail@markus-hoelzle.de>
2019-07-09 17:22:56 +0200

    [TASK] Add old testing framework dependency

--- Tests/Unit/Signal/FileIndexRepositoryTest.php
+++ Tests/Unit/Signal/FileIndexRepositoryTest.php
@@ -15,6 +15,7 @@ namespace AUS\AusDriverAmazonS3\Tests\Unit\Signal;
 use AUS\AusDriverAmazonS3\Driver\AmazonS3Driver;
 use AUS\AusDriverAmazonS3\Index\Extractor;
 use AUS\AusDriverAmazonS3\Signal\FileIndexRepository;
+use Prophecy\Prophecy\ObjectProphecy;
 use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
 use TYPO3\CMS\Core\Resource\File;
 use TYPO3\CMS\Core\Resource\ResourceStorage;
@@ -33,15 +34,10 @@ class FileIndexRepositoryTest extends UnitTestCase
     public function testRecordUpdatedOrCreatedHandleImageFileType()
     {
         $file = $this->prophesize(File::class)->reveal();
-        $storage = $this->prophesize(ResourceStorage::class);
-        $storage->getDriverType()->willReturn(AmazonS3Driver::DRIVER_TYPE);
-        $storage->getFile('foo/bar.file')->willReturn($file);
-        $extractor = $this->prophesize(Extractor::class);
-        $extractor->getImageDimensionsOfRemoteFile($file)->willReturn(null);
 
         $mock = $this->getMockBuilder(FileIndexRepository::class)->setMethods(['getStorage', 'getExtractor'])->getMock();
-        $mock->expects($this->exactly(1))->method('getStorage')->willReturn($storage->reveal());
-        $mock->expects($this->exactly(1))->method('getExtractor')->willReturn($extractor->reveal());
+        $mock->expects($this->exactly(1))->method('getStorage')->willReturn($this->getStorageProphecy($file)->reveal());
+        $mock->expects($this->exactly(1))->method('getExtractor')->willReturn($this->getExtractorProphecy($file)->reveal());
         $mock->recordUpdatedOrCreated([
             'type' => File::FILETYPE_IMAGE,
             'storage' => 42,
@@ -55,15 +51,10 @@ class FileIndexRepositoryTest extends UnitTestCase
     public function testRecordUpdatedOrCreatedDoNotHandleUnknownFileType()
     {
         $file = $this->prophesize(File::class)->reveal();
-        $storage = $this->prophesize(ResourceStorage::class);
-        $storage->getDriverType()->willReturn(AmazonS3Driver::DRIVER_TYPE);
-        $storage->getFile('foo/bar.file')->willReturn($file);
-        $extractor = $this->prophesize(Extractor::class);
-        $extractor->getImageDimensionsOfRemoteFile($file)->willReturn(null);
 
         $mock = $this->getMockBuilder(FileIndexRepository::class)->setMethods(['getStorage', 'getExtractor'])->getMock();
-        $mock->expects($this->exactly(0))->method('getStorage')->willReturn($storage->reveal());
-        $mock->expects($this->exactly(0))->method('getExtractor')->willReturn($extractor->reveal());
+        $mock->expects($this->exactly(0))->method('getStorage')->willReturn($this->getStorageProphecy($file)->reveal());
+        $mock->expects($this->exactly(0))->method('getExtractor')->willReturn($this->getExtractorProphecy($file)->reveal());
         $mock->recordUpdatedOrCreated([
             'type' => File::FILETYPE_UNKNOWN,
             'storage' => 42,
@@ -77,19 +68,37 @@ class FileIndexRepositoryTest extends UnitTestCase
     public function testRecordUpdatedOrCreatedDoNotHandleApplicationFileType()
     {
         $file = $this->prophesize(File::class)->reveal();
-        $storage = $this->prophesize(ResourceStorage::class);
-        $storage->getDriverType()->willReturn(AmazonS3Driver::DRIVER_TYPE);
-        $storage->getFile('foo/bar.file')->willReturn($file);
-        $extractor = $this->prophesize(Extractor::class);
-        $extractor->getImageDimensionsOfRemoteFile($file)->willReturn(null);
 
         $mock = $this->getMockBuilder(FileIndexRepository::class)->setMethods(['getStorage', 'getExtractor'])->getMock();
-        $mock->expects($this->exactly(0))->method('getStorage')->willReturn($storage->reveal());
-        $mock->expects($this->exactly(0))->method('getExtractor')->willReturn($extractor->reveal());
+        $mock->expects($this->exactly(0))->method('getStorage')->willReturn($this->getStorageProphecy($file)->reveal());
+        $mock->expects($this->exactly(0))->method('getExtractor')->willReturn($this->getExtractorProphecy($file)->reveal());
         $mock->recordUpdatedOrCreated([
             'type' => File::FILETYPE_APPLICATION,
             'storage' => 42,
             'identifier' => 'foo/bar.file',
         ]);
+    }
+
+    /**
+     * @param $file
+     * @return ObjectProphecy
+     */
+    protected function getStorageProphecy($file): ObjectProphecy
+    {
+        $storage = $this->prophesize(ResourceStorage::class);
+        $storage->getDriverType()->willReturn(AmazonS3Driver::DRIVER_TYPE);
+        $storage->getFile('foo/bar.file')->willReturn($file);
+        return $storage;
+    }
+
+    /**
+     * @param $file
+     * @return ObjectProphecy
+     */
+    protected function getExtractorProphecy($file): ObjectProphecy
+    {
+        $extractor = $this->prophesize(Extractor::class);
+        $extractor->getImageDimensionsOfRemoteFile($file)->willReturn(null);
+        return $extractor;
     }
 }

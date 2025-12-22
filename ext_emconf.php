84ee6868bff59c8e2b4651d704f120b213e61fdc
Lamm <s.lamm@andersundsehr.com>
2025-12-22 15:03:22 +0100

    Fixes for TYPO3 v13: Support TYPO3v13 only
    Drop support for TYPO3 v11 + v12
    
    Split off from commit 0f75bc9ef9 "Fixes for TYPO3 v13"

--- ext_emconf.php
+++ ext_emconf.php
@@ -17,7 +17,7 @@ $EM_CONF[$_EXTKEY] = [
         [
             'depends' =>
                 [
-                    'typo3' => '11.5.30-13.4.99',
+                    'typo3' => '13.4.0-13.4.99',
                 ],
             'conflicts' => [],
             'suggests' => [],

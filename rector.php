f7f82f133c18d4bd8c83b5dc3beccfb03b821450
Lamm <s.lamm@andersundsehr.com>
2025-12-22 15:01:44 +0100

    Fixes for TYPO3 v13: Add rector + phpstan configuration
    Split off from commit 0f75bc9ef9 "Fixes for TYPO3 v13"

---
+++ rector.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+use PLUS\GrumPHPConfig\RectorSettings;
+use Rector\Config\RectorConfig;
+use Rector\Caching\ValueObject\Storage\FileCacheStorage;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->parallel();
+    $rectorConfig->importNames();
+    $rectorConfig->importShortClasses();
+    $rectorConfig->cacheClass(FileCacheStorage::class);
+    $rectorConfig->cacheDirectory('./var/cache/rector');
+
+    $rectorConfig->paths(
+        array_filter(explode("\n", (string)shell_exec("git ls-files | xargs ls -d 2>/dev/null | grep -E '\.(php)$'")))
+    );
+
+    // define sets of rules
+    $rectorConfig->sets(
+        [
+            ...RectorSettings::sets(true),
+            ...RectorSettings::setsTypo3(false),
+        ]
+    );
+
+    // remove some rules
+    // ignore some files
+    $rectorConfig->skip(
+        [
+            ...RectorSettings::skip(),
+            ...RectorSettings::skipTypo3(),
+
+            /**
+             * rector should not touch these files
+             */
+            //__DIR__ . '/src/Example',
+            //__DIR__ . '/src/Example.php',
+        ]
+    );
+};
